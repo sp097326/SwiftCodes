f06914bfe5314827cb35f1e399a324e57b5c011c
sp097326 <is097326@hotmail.com>
2017-03-28 12:36:20 -0600

    EjerciciosSwift
    primeros ejercicios swift

---
+++ UdemyCoursePlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var cmdstr = "Another String"
+
+var name = "Alberto"
+
+var lastName = "Manzanilla"
+
+var fullName = "\(name) \(lastName)"
+
+var unNumero = 20
+
+var otroNumero = unNumero * 10 //las operaciones son directas
+
+//para efectos de concatenados en cadenas no imporrta mucho el tipo
+var concatenados = "una cadena + mas un numero:\(str) \(unNumero)"
+
+
+
+
