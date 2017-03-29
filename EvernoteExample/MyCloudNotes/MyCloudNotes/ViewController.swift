5e20cb0ab544d4aa20f087db66aff3f5d79f359a
sp097326 <is097326@hotmail.com>
2017-03-29 11:19:47 -0600

    some cleaning
    uses less comments removed

--- EvernoteExample/MyCloudNotes/MyCloudNotes/ViewController.swift
+++ EvernoteExample/MyCloudNotes/MyCloudNotes/ViewController.swift
@@ -12,24 +12,23 @@ import EvernoteSDK
 
 class ViewController: UIViewController {
 
+    //constants
     let key = "sp097326";
     let secret = "2ff61148b74fc723"
     let developerToken = "S=s50:U=54831e:E=1626a40b531:C=15b128f87c0:P=1cd:A=en-devtoken:V=2:H=7aff062bef69b8e5cb6903393812dd3c"
     let noteStore = "https://www.evernote.com/shard/s50/notestore"
 
+    //variables
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
     @IBAction func getCloudNotes(_ sender: UIButton) {
         print("Go get the notes");
         
-        //trying to get a connectionto Evernote
         //First setup you key session <this example has a developer token>
-        //this should be on a try/catch structure
         //ENSession.setSharedSessionConsumerKey(key,consumerSecret: secret, optionalHost: ENSessionHostSandbox);
         ENSession.setSharedSessionDeveloperToken(developerToken, noteStoreUrl: noteStore);
         print("executed setSharedSession");
@@ -39,7 +38,6 @@ class ViewController: UIViewController {
         }
         
         print("executed authenticate...");
-        
         
         
     }
