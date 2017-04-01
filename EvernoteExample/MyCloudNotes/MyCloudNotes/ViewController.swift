c8c5220347032bd3f2c4f697e743ec9e3828c386
sp097326 <is097326@hotmail.com>
2017-03-31 18:10:16 -0600

    Cleaning and carrying on
    Cleaned up comments, added a new label and cleaned it on start

--- EvernoteExample/MyCloudNotes/MyCloudNotes/ViewController.swift
+++ EvernoteExample/MyCloudNotes/MyCloudNotes/ViewController.swift
@@ -16,12 +16,16 @@ class ViewController: UIViewController {
     let key = "sp097326";
     let secret = "2ff61148b74fc723"
     let developerToken = "S=s50:U=54831e:E=1626a40b531:C=15b128f87c0:P=1cd:A=en-devtoken:V=2:H=7aff062bef69b8e5cb6903393812dd3c"
-    let noteStore = "https://www.evernote.com/shard/s50/notestore"
+    let noteStoreUrl = "https://www.evernote.com/shard/s50/notestore"
 
     //variables
+    @IBOutlet weak var noteBookList: UITextView!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        welcomeLabel.text = "";
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -30,12 +34,13 @@ class ViewController: UIViewController {
         
         //First setup you key session <this example has a developer token>
         //ENSession.setSharedSessionConsumerKey(key,consumerSecret: secret, optionalHost: ENSessionHostSandbox);
-        ENSession.setSharedSessionDeveloperToken(developerToken, noteStoreUrl: noteStore);
+        ENSession.setSharedSessionDeveloperToken(developerToken, noteStoreUrl: noteStoreUrl);
         print("executed setSharedSession");
         
         ENSession.shared.authenticate(with: self, preferRegistration: false) { (_error: Error?) in
             //print("Error in authenticate...");
         }
+        //welcomeLabel.text = ENSession.shared.userDisplayName
         
         print("executed authenticate...");
         
