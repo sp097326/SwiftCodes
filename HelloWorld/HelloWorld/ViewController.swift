37db2abd8c9bee3820f27827f2fc1c9bf1fe0a2c
sp097326 <is097326@hotmail.com>
2017-04-11 19:06:24 -0500

    Finished helloworld
    Udemy excursive finished

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var buttonPressed: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        let alertController : UIAlertController = UIAlertController(title: "Hola, me has pulsado", message: "Botón presionado", preferredStyle: .alert)
+        
+        let okAction : UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
 
 
