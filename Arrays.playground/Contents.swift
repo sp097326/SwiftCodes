818fda9de95289a81629fec42448114f74467139
sp097326 <is097326@hotmail.com>
2017-05-16 19:32:09 -0500

    Playground de Arrays
    Playground de arrays, para continuar con la secuencia del curso de udemy

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,40 @@
+var age : Int = 39
+
+var name : String  = "Alberto Manzanilla"
+
+
+
+var shoppingList = ["Patatas","Huevo", "Cebolla", "Pimientos"]
+var shoppingListDeclared : [String] = ["algo", "segundo"]
+
+var luckyNumbers = [7,11,5,1,13]
+
+var weight = [68.9, 49.5, 80.0]
+
+var activeItems = [true, false, false, true, true]
+
+
+var newArray : [String] = []
+
+var otherArray : [String] //así estaría en el limbo, no está creado
+
+//todo esto puede estar declarado como constante con Let
+
+shoppingList.count
+
+shoppingList.append("Plátano")
+
+shoppingList.count
+
+shoppingList.append("5 litros de chela")
+
+shoppingList.count
+
+luckyNumbers.append(3)
+
+luckyNumbers.count
+
+
+
+
+
