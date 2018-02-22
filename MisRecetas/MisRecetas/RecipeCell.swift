479b3a49e50058222df93552505f8a2f0de25690
sp097326 <is097326@hotmail.com>
2018-02-22 11:05:48 -0600

    Continuando con curso
    Seguimos con el curso de apple, agregado de imagen y labels y un Custom
    Row

---
+++ MisRecetas/MisRecetas/RecipeCell.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeCell.swift
+//  MisRecetas
+//
+//  Created by swiftdev on 2/21/18.
+//  Copyright © 2018 swiftdevelopment. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
